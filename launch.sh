32752003ea76012551f0d3660cfadec36c21c7e4
Travis Reed <tdreed@gmail.com>
2024-05-29 11:19:52 -0700

    A bit of namespacing

--- launch.sh
+++ launch.sh
@@ -2,8 +2,8 @@
 
 if [ -e $(whereis ros2 | awk '{print $2}') ]; then
     # Sometimes we're launched on a non-ROS2 host
-    if [ -e /psyche/src/psyche/launch/$(hostname).launch.py ]; then
-        ros2 launch psyche $(hostname).launch.py
+    if [ -e /psyche/src/psyche/launch/by_host_$(hostname).launch.py ]; then
+        ros2 launch psyche by_host_$(hostname).launch.py
     fi
 else
     if [ -e /psyche/launch/$(hostname).sh ]; then
