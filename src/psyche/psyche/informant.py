260a319962e61ba6fff9aac348605c45a265c057
Travis Reed <tdreed@gmail.com>
2024-06-02 16:00:08 -0700

    Changing terms a bit

--- src/psyche/psyche/informant.py
+++ src/psyche/psyche/informant.py
@@ -19,12 +19,11 @@ from rclpy.node import Node
 from rclpy.action import ActionServer
 
 
-class SelfInformedLanguageProcessor(LanguageProcessor):
+class Informant(LanguageProcessor):
     """
-    A language processor that wraps a langchain with a RAG system that shows itself its own source code
+    A language processor with a chain that uses the RAG system to answer questions.
     """
-    def setup_chain(self):
-        # TODO: Set a stable source path
+    def setup_documents(self):
         current_file_path = os.path.abspath(__file__)
         self_dir = os.path.dirname(current_file_path)
         supposed_source_path = os.path.dirname(os.path.abspath(self_dir + '/../../'))
@@ -37,7 +36,11 @@ class SelfInformedLanguageProcessor(LanguageProcessor):
             language=Language.PYTHON, chunk_size=50, chunk_overlap=0
         )
         self.split_docs = self.text_splitter.split_documents(self.raw_docs)
+        self.documents = self.split_docs
+        self.get_logger().info('Documents loaded and split.')        
 
+    def setup_embeddings(self):
+        # TODO: These should be separate parameters from the llm itself
         model = self.get_parameter('model').get_parameter_value().string_value
         base_url = self.get_parameter('base_url').get_parameter_value().string_value
         model_type = self.get_parameter('model_type').get_parameter_value().string_value
@@ -45,22 +48,29 @@ class SelfInformedLanguageProcessor(LanguageProcessor):
         if model_type == 'openai':
             self.real_embeddings = OpenAIEmbeddings()
         else:
+            # TODO: Add support for specifying the embedding model
             self.real_embeddings = OllamaEmbeddings(model="mxbai-embed-large", base_url=base_url)
         
         # TODO: Use a stable cache path
         self.embeddings_store = LocalFileStore("/tmp/embeddings_cache/")
         self.embeddings = CacheBackedEmbeddings.from_bytes_store(
             self.real_embeddings, self.embeddings_store, namespace=self.real_embeddings.model
-        )
+        )        
+    
+    def setup_retriever(self):
         self.db = FAISS.from_documents(self.split_docs, self.real_embeddings)
         self.retriever = self.db.as_retriever()
-        self.rag_prompt = PromptTemplate.from_template("""You are an assistant for answering questions about your own source code. Use the following pieces of retrieved source code to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.
-
-        Question: {question} 
-
-        Context: {context} 
-
-        Answer: """)
+    
+    def setup_chain(self):
+        self.setup_documents()
+        self.setup_embeddings()
+        self.setup_retriever()
+        
+        self.declare_parameter('rag_prompt_template', 'You are an assistant for answering questions about your own source code. Use the following pieces of retrieved source code to answer the question. If you don\'t know the answer, just say that you don\'t know. Use three sentences maximum and keep the answer concise.\n\nQuestion: {question}\n\nContext: {context}\n\nAnswer: ')
+        rag_prompt_template = self.get_parameter('rag_prompt_template').get_parameter_value().string_value
+        self.rag_prompt = PromptTemplate.from_template(rag_prompt_template)
+        
+        # TODO: These depend on the prompt template
         self.rag = {"context": self.retriever, "question": RunnablePassthrough()} | self.rag_prompt | self.llm | self.output_parser
         self.chain = self.rag
         self.get_logger().info('Processing chain created with RAG system.')
@@ -68,7 +78,7 @@ class SelfInformedLanguageProcessor(LanguageProcessor):
 def main(args=None):
     rclpy.init(args=args)
 
-    node = SelfInformedLanguageProcessor(
+    node = Informant(
         'autognosis_language_processor',
         'to_thine_own_self_be_true'
     )
