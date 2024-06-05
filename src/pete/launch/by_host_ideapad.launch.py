0187b121d5f1a4d5120079b107797ff95aba7e4f
Travis Reed <tdreed@gmail.com>
2024-06-05 05:23:52 +0000

    For local testing

---
+++ src/pete/launch/by_host_ideapad.launch.py
@@ -0,0 +1,21 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import PathJoinSubstitution
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
+
+# This is onboard the robot. As much as possible should run here
+def generate_launch_description():
+    return LaunchDescription([
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([
+                    PathJoinSubstitution([
+                        FindPackageShare('pete'),
+                        'launch',
+                        f'{script}.launch.py'
+                    ])
+                ])
+            ) for script in ['body', 'combobulation', 'comprehension', 'identity', 'memory', 'lpu2', 'sentience', 'speech']
+    ])
