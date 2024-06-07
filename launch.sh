5312df9f36f1e8fb272a161f53bd37d703be6618
Travis Reed <tdreed@gmail.com>
2024-06-07 15:08:52 -0700

    🐛 (launch.sh): Move environment variable exports and source commands inside Docker container check 🔧 (hindbrain.sh): Update file permissions to make it executable The environment variable exports and source commands were moved inside the Docker container check in the launch.sh file to ensure they are only executed when the script is running inside a Docker container. This change was made to prevent potential conflicts or errors when the script is run outside of a Docker container. The file permissions of hindbrain.sh were updated to make it executable, allowing it to be run as a script without needing to explicitly call a shell command.

--- launch.sh
+++ launch.sh
@@ -5,19 +5,15 @@
 # "pete" is the robot who _hosts_ the psyche toolset
 export HOST_PKG=pete
 
-export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
-export ROS_DOMAIN_ID=42
-
-source /opt/ros/$ROS_DISTRO/setup.bash
-source /psyche/install/setup.bash
-
 ### Check if running inside a Docker container
 if [ -e "/ros_entrypoint.sh" ]; then
-    echo "Outside docker node, use base launch shell scripts"
-    if [ -e /psyche/launch/$(hostname).sh ]; then
-        /psyche/launch/$(hostname).sh
-    fi
-else
+
+    export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+    export ROS_DOMAIN_ID=42
+
+    source /opt/ros/$ROS_DISTRO/setup.bash
+    source /psyche/install/setup.bash
+
     echo "Inside docker node, use $HOST_PKG launch files"
     echo "Looking for /psyche/src/$HOST_PKG/launch/by_host_$(hostname).launch.py"
     if [ -e /psyche/src/$HOST_PKG/launch/by_host_$(hostname).launch.py ]; then
@@ -27,6 +23,15 @@ else
     
     echo "Running the autoexec launch file"
     ros2 launch $HOST_PKG autoexec.launch.py
+
+else
+
+    echo "Outside docker node, use base launch shell scripts"
+    if [ -e /psyche/launch/$(hostname).sh ]; then
+        /psyche/launch/$(hostname).sh
+    fi
+
+
 fi
 
 ### If we get here, just spin
