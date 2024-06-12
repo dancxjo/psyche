87623ba206577091039550d107aedaa3eac5c54f
Travis Reed <tdreed@gmail.com>
2024-06-12 04:49:07 +0000

    launch another vlpu

--- src/r1/launch/llms.launch.py
+++ src/r1/launch/llms.launch.py
@@ -10,28 +10,28 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def generate_launch_description():
     return LaunchDescription([
-        Node(
-            package="psyche",
-            executable="lpu",
-            name="cheap_lpu",
-            output="screen",
-            parameters=[
-                {"model_type": "openai"},
-                {"model": "gpt-3.5-turbo"},
-                {"action_server_name": "instruct_cheap"}
-            ],
-        ),    
-        Node(
-            package="psyche",
-            executable="lpu",
-            name="expensive_lpu",
-            output="screen",
-            parameters=[
-                {"model_type": "openai"},
-                {"model": "gpt-4"},
-                {"action_server_name": "instruct_expensive"}
-            ],
-        ),    
+        # Node(
+        #     package="psyche",
+        #     executable="lpu",
+        #     name="cheap_lpu",
+        #     output="screen",
+        #     parameters=[
+        #         {"model_type": "openai"},
+        #         {"model": "gpt-3.5-turbo"},
+        #         {"action_server_name": "instruct_cheap"}
+        #     ],
+        # ),    
+        # Node(
+        #     package="psyche",
+        #     executable="lpu",
+        #     name="expensive_lpu",
+        #     output="screen",
+        #     parameters=[
+        #         {"model_type": "openai"},
+        #         {"model": "gpt-4"},
+        #         {"action_server_name": "instruct_expensive"}
+        #     ],
+        # ),    
         Node(
             package="psyche",
             executable="lpu",
@@ -39,8 +39,19 @@ def generate_launch_description():
             output="screen",
             parameters=[
                 {"model": "llama3:instruct"},
-                {"base_url": "http://192.168.0.133:11434"},
+                {"base_url": "http://192.168.0.4:11434"},
                 {"action_server_name": "instruct"}
+            ],
+        ),
+        Node(
+            package="psyche",
+            executable="vlpu",
+            name="vision_lpu",
+            output="screen",
+            parameters=[
+                {"model": "llava:13b"},
+                {"base_url": "http://192.168.0.4:11434"},
+                {"action_server_name": "inspect"}
             ],
         ),
     ])
