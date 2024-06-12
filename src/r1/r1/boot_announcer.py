56b0d4e13f574e1f7824d5b1e39daf49569bc195
Travis Reed <tdreed@gmail.com>
2024-06-12 14:27:44 -0700

    feat: Add timer callback to announce boot multiple times
    This commit adds a timer callback function to the `BootAnnouncer` class in the `boot_announcer.py` file. The timer callback is triggered every 30 seconds and announces the boot status. It keeps track of the number of times the announcement has been made and increments it with each callback. This enhancement improves the user experience by providing periodic updates during the boot process.

--- src/r1/r1/boot_announcer.py
+++ src/r1/r1/boot_announcer.py
@@ -8,10 +8,17 @@ class BootAnnouncer(Node):
         super().__init__('boot_announcer')
         self.voice = self.create_publisher(String, 'voice', 10)
         self.say(String(data='Hello, I am R1! Just a moment while I wake up.'))
+        self.timer = self.create_timer(30.0, self.timer_callback)
+        self.times = 1
         
     def say(self, msg):
         self.get_logger().debug('Saying: %s' % msg.data)
         self.voice.publish(msg)
+        
+    def timer_callback(self):
+        self.get_logger().info('Waking up...')
+        self.say(String(data=f'I\'ve already announced this {self.times} times!'))
+        self.times += 1
 
 def main(args=None):
     rclpy.init(args=args)
