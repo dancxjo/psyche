4262fd9c404f36ee1b03315bae949c6bd075eef7
Travis Reed <tdreed@gmail.com>
2024-06-16 09:04:44 -0700

    ♻️ (by_host_forebrain.launch.py): Remove LLMS start nodes The LLMS start nodes were removed from the launch file as they were causing redundancy. The nodes were not necessary for the current implementation and were causing confusion and potential conflicts. The removal simplifies the launch process and makes the code cleaner and easier to maintain.

--- src/r1/launch/by_host_forebrain.launch.py
+++ src/r1/launch/by_host_forebrain.launch.py
@@ -19,29 +19,6 @@ from launch.actions import IncludeLaunchDescription, ExecuteProcess  # Import Ex
 
 def generate_launch_description():
     return LaunchDescription([
-        # Start the LLMS
-        Node(
-            package="psyche",
-            executable="lpu",
-            name="basic_lpu",
-            output="screen",
-            parameters=[
-                {"model": "llama3:instruct"},
-                {"base_url": "http://127.0.0.1:11434"},
-                {"action_server_name": "instruct_per_se"}
-            ],
-        ),
-        Node(
-            package="psyche",
-            executable="vlpu",
-            name="vision_lpu",
-            output="screen",
-            parameters=[
-                {"model": "llava:13b"},
-                {"base_url": "http://127.0.0.1:11434"},
-                {"action_server_name": "inspect_per_se"}
-            ],
-        ),
         Node(
             package="psyche",
             executable="transcribe_speech",
