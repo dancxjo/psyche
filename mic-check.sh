85eee59f75513f66aed7023372e3977e598dd611
Travis Reed <tdreed@gmail.com>
2024-06-17 17:17:45 -0700

    🐛 (mic-check.sh): Replace hardcoded message with command line arguments for flexibility The hardcoded message in the mic-check.sh script has been replaced with command line arguments. This allows for more flexibility as the message can now be specified at runtime, making the script more versatile and adaptable to different use cases.

--- mic-check.sh
+++ mic-check.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /root/.bashrc
-ros2 topic pub /voice std_msgs/msg/String "data: 'Hello my baby hello my darling hello my ragtime gal'"
+ros2 topic pub /voice std_msgs/msg/String "data: \"$@\""
